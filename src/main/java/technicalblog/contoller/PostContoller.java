7565900be3cfe284ecfb1924ad9dd44b3d1d77a8
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2020-10-08 06:05:33 +0530

    Added login feature and displaying single posts. Completed till login feature 3

--- src/main/java/technicalblog/contoller/PostContoller.java
+++ src/main/java/technicalblog/contoller/PostContoller.java
@@ -1,4 +1,25 @@
 package technicalblog.contoller;
 
+import javafx.geometry.Pos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
+
+@Controller
 public class PostContoller {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("posts")
+    public String getUserPosts(Model model){
+        ArrayList<Post> posts = postService.getOnePost();
+        model.addAttribute("posts", posts);
+        return "posts";
+    }
 }
