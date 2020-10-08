395559ffa6b3801c890c30a984dae9f8030bbc78
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2020-10-08 06:29:52 +0530

    Added logout functionality. Login Feature 4 completed

--- src/main/java/technicalblog/contoller/UserController.java
+++ src/main/java/technicalblog/contoller/UserController.java
@@ -1,12 +1,21 @@
 package technicalblog.contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import technicalblog.model.Post;
 import technicalblog.model.User;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PostService postService;
 
     @RequestMapping("users/login")
     public String login(){
@@ -21,5 +30,12 @@ public class UserController {
     @RequestMapping(value = "users/login", method = RequestMethod.POST)
     public String loginUser(User user){
         return "redirect:/posts";
+    }
+
+    @RequestMapping(value = "users/logout", method = RequestMethod.POST)
+    public String logout(Model model) {
+        ArrayList<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
+        return "index";
     }
 }
